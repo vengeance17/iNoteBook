f311056e605f2d2193f928a6157d71729c77dd99
Bruce Wayne <108128942+vengeance17@users.noreply.github.com>
2022-09-05 20:19:45 +0530

    Update fetchuser.js

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'Divyanshuisagoodb$oy';
+const JWT_SECRET = 'Harryisagoodb$oy';
 const fetchuser = (req,res,next) =>
 {
     //Get the user from the jwt token and add id to req object
